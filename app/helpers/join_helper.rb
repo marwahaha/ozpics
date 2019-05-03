2143c33e0d2d1ebaa382d751e27a60575a0eb797
jingli <jingli.claire@gmail.com>
2019-05-03 16:37:24 +1000

    popover for sign in and sign up function

--- app/helpers/join_helper.rb
+++ app/helpers/join_helper.rb
@@ -1,2 +1,36 @@
 module JoinHelper
+
+#buyer
+  def buyer_resource_name
+    :buyer
+  end
+
+  def buyer_resource
+    @resource ||= Buyer.new
+  end
+
+  def buyer_resource_class
+    Buyer
+  end
+
+  def buyer_devise_mapping
+    @devise_mapping ||= Devise.mappings[:buyer]
+  end
+
+#seller
+  def seller_resource_name
+    :seller
+  end
+ 
+  def seller_resource
+    @resource ||= Seller.new
+  end
+
+  def seller_resource_class
+    Seller
+  end
+ 
+  def seller_devise_mapping
+    @devise_mapping ||= Devise.mappings[:seller]
+  end
 end
