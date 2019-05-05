d609588f85dbd972d92b1cc51d6e35f2e764f56c
jingli <jingli.claire@gmail.com>
2019-05-05 15:36:26 +1000

    add cart link to navbar, add delete line item function to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,14 +1,21 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create, :destroy]
 
   def create
     photo = Photo.find(params[:photo_id])
     @line_item = @cart.line_items.build(photo: photo)
-
     if @line_item.save
-      redirect_to @line_item.cart, notice: 'Photo was successfully added to cart!'
+      redirect_to @line_item.cart, notice: 'Successfully added to cart!'
     end
+  end
+
+  def destroy
+    @line_item = @cart.line_items.find(params[:id])
+    @line_item.destroy
+
+    redirect_to @cart, notice: 'Successfully deleted from cart!'
+
   end
 
   private
