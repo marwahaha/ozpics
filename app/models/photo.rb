f80c7c3feb32d2877cf0302b478d4458a64e8881
jingli <jingli.claire@gmail.com>
2019-05-10 11:33:41 +1000

    Fixed photo deleted from order with placeholder image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,7 @@ class Photo < ApplicationRecord
 
   belongs_to :seller
   belongs_to :category
-  has_many :line_items, dependent: :destroy
+  has_many :line_items, dependent: :nullify
 
   has_one_attached :uploaded_image
 
